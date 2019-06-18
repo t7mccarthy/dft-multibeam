d1ad0e45d2d2898221a1e28dfa363a0f999a1f3a
t7mccarthy <tom@toms-mbp.attlocal.net>
2019-06-17 17:57:22 -0700

    its alive

--- 3D_FFT_multibeam.py
+++ 3D_FFT_multibeam.py
@@ -83,70 +83,101 @@ def dft():
 
 
 
-# def get_array_factor (k, dx, dy, theta, phi, n, ax, ay):
-#     sum1 = 0
-#     sum2 = 0
-#     # print (theta)
-#     a = math.sin(theta)
-#     for i in range(-n, n):
-#         sum1 += cmath.exp(1j*i*(k*dx*math.sin(theta)*math.cos(phi)-ax))
-#         sum2 += cmath.exp(1j*i*(k*dy*math.sin(theta)*math.sin(phi)-ay))
-#     return abs(sum1 * sum2)
+def get_array_factor (theta, phi):
+    sum = 0
+    for u in range(M):
+        for v in range(N):
+            sum += (F_uv[u][v]/F_uv[0][0]) * cmath.exp(1j * wave_num * math.sin(theta) * (u * dx * math.cos(phi) + v * dy * math.sin(phi)))
+
+    return abs(sum)
+    # for i in range(-n, n):
+    #     sum1 += cmath.exp(1j*i*(k*dx*math.sin(theta)*math.cos(phi)-ax))
+    #     sum2 += cmath.exp(1j*i*(k*dy*math.sin(theta)*math.sin(phi)-ay))
+    # return abs(sum1 * sum2)
 
 def main():
     sample_angles()
-    # print (angles)
     peak_approximator()
     dft()
-
     print (F_uv)
-    pts = []
 
-    for a in range(M):
-        plt.scatter(theta_xy[a], phi_xy[a], color = 'red', marker = 'o')
-        for b in range(M):
-            pts.append((theta_xy[a][b], phi_xy[a][b]))
-
-    pts.append((30, 30))
-
-
-    plt.scatter([angles[15][0]], [angles[15][1]], marker='o', color = 'blue')
-    plt.scatter([30], [30], marker='s', color = 'yellow')
-    # print (pts == angles)
-    # plt.plot(range(M), theta_xy, color = 'red', marker = 'o', label = 'Theta samples')
-    # plt.plot(range(M), phi_xy, color = 'blue', marker = 'o', label = 'Phi samples')
-
-    # plt.legend(loc='best', fontsize='small')
-    plt.title('Angle Samples')
-    plt.xlabel('Theta')
-    plt.ylabel('Phi')
-    plt.show()
-
-    xs = []
-    ys = []
-    zs = []
-    R = 30
-    for pt in pts:
-        THETA = math.radians(pt[0])
-        PHI = math.radians(pt[1])
-        z = R * math.cos(THETA)
-        # if z >= 0:
-        xs.append(R * math.sin(THETA) * math.cos(PHI))
-        ys.append(R * math.sin(THETA) * math.sin(PHI))
-        zs.append((R * math.cos(THETA)))
+    # f2 = np.vectorize(get_array_factor)
+    dim = 100
+    theta, phi = np.linspace(0, 2 * np.pi, dim), np.linspace(0, np.pi, dim)
+    THETA, PHI = np.meshgrid(theta, phi)
+    r = [[0]*dim for _ in range(dim)]
+    for i in range(dim):
+        for j in range(dim):
+            # print("*******************************************************")
+            # print (math.degrees(THETA[i][j]), math.degrees(PHI[i][j]))
+            r[i][j]=get_array_factor(THETA[i][j], PHI[i][j])
+            # print (math.degrees(r[i][j]))
+    R = np.array(r)
+    print (np.shape(THETA))
+    print (np.shape(PHI))
+    # R = np.cos(PHI**2)
+    # R = f2(THETA, PHI, k, dx, dy, n, alphax, alphay)
+    print (np.shape(R))
+    print (R)
+    X = R * np.sin(THETA) * np.cos(PHI)
+    Y = R * np.sin(THETA) * np.sin(PHI)
+    Z = R * np.cos(THETA)
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    # ax.mplt3d([0, 0], [0,0], [-10, 10], linewidth=2, color='r')
-
-    ax.scatter(xs, ys, zs, marker='o')
-    ax.scatter([xs[15]], [ys[15]], [zs[15]], marker='s', color = 'yellow')
-    ax.scatter([xs[-1]], [ys[-1]], [zs[-1]], marker='s', color = 'red')
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
+    ax = fig.add_subplot(1,1,1, projection='3d')
+    plot = ax.plot_surface(X, Y, Z)
+        # X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('jet'),
+        # linewidth=0, antialiased=False, alpha=0.5)
 
     plt.show()
+
+    # print (F_uv)
+    # pts = []
+    #
+    # for a in range(M):
+    #     plt.scatter(theta_xy[a], phi_xy[a], color = 'red', marker = 'o')
+    #     for b in range(M):
+    #         pts.append((theta_xy[a][b], phi_xy[a][b]))
+    #
+    # pts.append((30, 30))
+    #
+    #
+    # plt.scatter([angles[15][0]], [angles[15][1]], marker='o', color = 'blue')
+    # plt.scatter([30], [30], marker='s', color = 'yellow')
+    # # print (pts == angles)
+    # # plt.plot(range(M), theta_xy, color = 'red', marker = 'o', label = 'Theta samples')
+    # # plt.plot(range(M), phi_xy, color = 'blue', marker = 'o', label = 'Phi samples')
+    #
+    # # plt.legend(loc='best', fontsize='small')
+    # plt.title('Angle Samples')
+    # plt.xlabel('Theta')
+    # plt.ylabel('Phi')
+    # plt.show()
+    #
+    # xs = []
+    # ys = []
+    # zs = []
+    # R = 30
+    # for pt in pts:
+    #     THETA = math.radians(pt[0])
+    #     PHI = math.radians(pt[1])
+    #     z = R * math.cos(THETA)
+    #     # if z >= 0:
+    #     xs.append(R * math.sin(THETA) * math.cos(PHI))
+    #     ys.append(R * math.sin(THETA) * math.sin(PHI))
+    #     zs.append((R * math.cos(THETA)))
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # # ax.mplt3d([0, 0], [0,0], [-10, 10], linewidth=2, color='r')
+    #
+    # ax.scatter(xs, ys, zs, marker='o')
+    # ax.scatter([xs[15]], [ys[15]], [zs[15]], marker='s', color = 'yellow')
+    # ax.scatter([xs[-1]], [ys[-1]], [zs[-1]], marker='s', color = 'red')
+    #
+    # ax.set_xlabel('X Label')
+    # ax.set_ylabel('Y Label')
+    # ax.set_zlabel('Z Label')
+    #
+    # plt.show()
 
 
 if __name__ == '__main__':
